32cea0bb3a3cc4787fa3829d5588899c3e6c49e6
LucaSalmi <luca.salmi90@gmail.com>
2021-10-11 16:33:14 +0200

    added back button

--- app/src/main/java/com/khystudent/restaurangsmeny/Desserts.java
+++ app/src/main/java/com/khystudent/restaurangsmeny/Desserts.java
@@ -2,13 +2,28 @@ package com.khystudent.restaurangsmeny;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Desserts extends AppCompatActivity {
+
+    ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desserts);
+
+        back = findViewById(R.id.return_button);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goBack = new Intent(Desserts.this, MainActivity.class);
+                startActivity(goBack);
+            }
+        });
     }
 }
--- app/src/main/java/com/khystudent/restaurangsmeny/Starters.java
+++ app/src/main/java/com/khystudent/restaurangsmeny/Starters.java
@@ -2,13 +2,27 @@ package com.khystudent.restaurangsmeny;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Starters extends AppCompatActivity {
+    ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starters);
+
+        back = findViewById(R.id.return_button);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goBack = new Intent(Starters.this, MainActivity.class);
+                startActivity(goBack);
+            }
+        });
     }
 }
